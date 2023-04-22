0fcfae122ee11bc285f818441aafe8a007705db1
Ismayil Taghi-Zada <itagizade@yahoo.com>
2023-04-23 00:40:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -16,16 +16,15 @@ def loginMySAT():
     driver.refresh()
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/main/div/div/div/div/div/div/div/div/div/a'))).click() # Click the first continue button
     sleep(2)
-
     elementIdpUsername = driver.find_element(By.XPATH, '//*[@id="idp-discovery-username"]') # Identify username inout field
     elementIdpUsername.clear()
-    elementIdpUsername.send_keys("alibbas177@gmail.com") # Enter required email, to be prompted in next update if required
+    elementIdpUsername.send_keys("aibragim008@gmail.com") # Enter required email, to be prompted in next update if required
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))).click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="idp-discovery-submit"]'))).click() # Trigger click event on Next "submit" type button after entering email
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="okta-signin-password"]')))
     elementIdpPasswd = driver.find_element(By.XPATH, '//*[@id="okta-signin-password"]') # Identify Password input field
     elementIdpPasswd.clear()
-    elementIdpPasswd.send_keys("Bb(123456)") # Enter required password TODO: remove password before pushing to GitHub!!!!!!
+    elementIdpPasswd.send_keys("Vv123456") # Enter required password TODO: remove password before pushing to GitHub!!!!!!
     driver.find_element(By.XPATH, '//*[@id="okta-signin-submit"]').click() # Trigger click event to submit password and email
     #print(driver.title)
 
