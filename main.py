dcb5cd736eae1fa8dea3607b047c88708ae20e50
Ismayil Taghi-Zada <itagizade@yahoo.com>
2023-04-25 01:43:39 +0400

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def loginMySAT():
     sleep(2)
     elementIdpUsername = driver.find_element(By.XPATH, '//*[@id="idp-discovery-username"]') # Identify username inout field
     elementIdpUsername.clear()
-    elementIdpUsername.send_keys("aibragim008@gmail.com") # Enter required email, to be prompted in next update if required
+    elementIdpUsername.send_keys("alibbas177@gmail.com") # Enter required email, to be prompted in next update if required
     
     try:
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="idp-discovery-submit"]'))).click() # Trigger click event on Next "submit" type button after entering email
@@ -29,18 +29,13 @@ def loginMySAT():
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="okta-signin-password"]')))
         elementIdpPasswd = driver.find_element(By.XPATH, '//*[@id="okta-signin-password"]') # Identify Password input field
         elementIdpPasswd.clear()
-        elementIdpPasswd.send_keys("Vv123456!") # Enter required password TODO: remove password before pushing to GitHub!!!!!!
+        elementIdpPasswd.send_keys("Bb(123456)") # Enter required password TODO: remove password before pushing to GitHub!!!!!!
         driver.find_element(By.XPATH, '//*[@id="okta-signin-submit"]').click() # Trigger click event to submit password and email
         #print(driver.title)
 
 
 def satreg():
-    try:
-        WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qc-id-header-register-button"]'))).click()
-    except TimeoutException:
-        print(f"Register button timeout, {TimeoutException}")
-        driver.get("https://mysat.collegeboard.org/dashboard")
-        satreg()
+    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qc-id-header-register-button"]'))).click()
     try:
         WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div/div[2]/div/div[6]/div/div/div[3]/div/div/div[2]/div[2]/button'))).click()
     except TimeoutException:
