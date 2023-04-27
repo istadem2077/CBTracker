19a7d28914715b24d8e6e5097a98022905b6ba84
Ismayil Taghi-Zada <itagizade@yahoo.com>
2023-04-27 11:00:16 +0400

    temporary

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ import tgmessage
 import schoolcount
 # from xvfbwrapper import Xvfb
  # Initiate Chrome Browser
-def loginMySAT(logins):
+def loginMySAT():
     driver.get("https://mysat.collegeboard.org/") # Login to website
     driver.refresh()
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/main/div/div/div/div/div/div/div/div/div/a'))).click() # Click the first continue button
     sleep(2)
     elementIdpUsername = driver.find_element(By.XPATH, '//*[@id="idp-discovery-username"]') # Identify username inout field
     elementIdpUsername.clear()
-    elementIdpUsername.send_keys(logins[0]) # Enter required email, to be prompted in next update if required
+    elementIdpUsername.send_keys("xanbutayevrafail@gmail.com") # Enter required email, to be prompted in next update if required
     
     try:
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="idp-discovery-submit"]'))).click() # Trigger click event on Next "submit" type button after entering email
@@ -29,7 +29,7 @@ def loginMySAT(logins):
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="okta-signin-password"]')))
         elementIdpPasswd = driver.find_element(By.XPATH, '//*[@id="okta-signin-password"]') # Identify Password input field
         elementIdpPasswd.clear()
-        elementIdpPasswd.send_keys(logins[1]) # Enter required password TODO: remove password before pushing to GitHub!!!!!!
+        elementIdpPasswd.send_keys("Rr((152535))") # Enter required password TODO: remove password before pushing to GitHub!!!!!!
         driver.find_element(By.XPATH, '//*[@id="okta-signin-submit"]').click() # Trigger click event to submit password and email
         #print(driver.title)
 
@@ -118,37 +118,36 @@ op.add_argument("--disable-browser-side-navigation")
 op.add_argument("--no-sandbox")
 op.add_argument("--disable-dev-shm-usage")
 counter = 0
-logincreds = [[]] # logincreds[iterator][0] - email; logincreds[iterator][1]
+#logincreds = [[]] # logincreds[iterator][0] - email; logincreds[iterator][1]
 iterator = 0
 while(1):
-    try:
-        driver = WD.Chrome(options=op)
-        print("Logging in")
-        loginMySAT(logincreds[iterator])
-        print("Entering registration")
-        satreg()
-        print("Choosing test date:")
-        chooseTestDate()
-        print("Finding test centers")
-        findtestcenter()
-        checkSchools(schoolcount.stripresult(jun_3))
-        while(1): # Infinite loop which breaks if an exception appears
-            try:
-                refreshTestCenter()
-            except:
-                break
-            else:
-                checkSchools(schoolcount.stripresult(jun_3))            
-        #sleep(60)
-        print("Restarting the loop")
-    except TimeoutException:
-        print(TimeoutException)
-        print(tgmessage.telegram_sendmessage(5670908383, f"{ctime(time())}, {TimeoutException}"))
-        if driver.page_source.__contains__("Unable to sign in"):
-            logincreds.pop(iterator)
-            iterator += 1
-            print(tgmessage.telegram_sendmessage(5670908383, f"Login changed, emails left: {len(logincreds)}"))
-        continue
+    driver = WD.Chrome(options=op)
+    print("Logging in")
+    loginMySAT()
+    print("Entering registration")
+    satreg()
+    print("Choosing test date:")
+    chooseTestDate()
+    print("Finding test centers")
+    findtestcenter()
+    checkSchools(schoolcount.stripresult(jun_3))
+    while(1): # Infinite loop which breaks if an exception appears
+        try:
+            refreshTestCenter()
+        except:
+            break
+        else:
+            checkSchools(schoolcount.stripresult(jun_3))            
+    #sleep(60)
+    print("Restarting the loop")
+    #except TimeoutException:
+    #    print(TimeoutException)
+    #    print(tgmessage.telegram_sendmessage(5670908383, f"{ctime(time())}, {TimeoutException}"))
+    #    if driver.page_source.__contains__("Unable to sign in"):
+    #        logincreds.pop(iterator)
+    #        iterator += 1
+    #        print(tgmessage.telegram_sendmessage(5670908383, f"Login changed, emails left: {len(logincreds)}"))
+    #    continue
     #except ElementClickInterceptedException:
     #    print(ElementClickInterceptedException)
     #    continue
