9e441a931e7d080184d019bd8042a52d2c858c85
Ismayil Taghi-Zada <46377455+istadem2077@users.noreply.github.com>
2023-05-31 21:20:46 +0400

    Try using proxy

--- main.py
+++ main.py
@@ -121,6 +121,8 @@ op = WD.ChromeOptions()
 op.add_argument("--disable-browser-side-navigation")
 op.add_argument("--no-sandbox")
 op.add_argument("--disable-dev-shm-usage")
+PROXY="144.217.197.151"
+op.add_argument(f"--proxy-server={PROXY}")
 counter = 0
 #logincreds = [[]] # logincreds[iterator][0] - email; logincreds[iterator][1]
 iterator = 0
